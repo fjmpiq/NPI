0e31879a781b438800cfd44450eed9d663d1970f
Pablo <pbaeyens31@gmail.com>
2018-10-16 18:31:04 +0200

    Actualiza código de PubNub - Quitado código que creo que no hacía falta. - Reestructurado código

--- pubnub.js
+++ pubnub.js
@@ -1,59 +1,47 @@
 export default (request, response) => {
-    const pubnub = require('pubnub');
-    const kvstore = require('kvstore');
     const xhr = require('xhr');
-    
-    let headersObject = request.headers;
-    let paramsObject = request.params;
-    let methodString = request.method;
-    let bodyString = request.body;
+    let any = JSON.parse(request.body).queryResult.parameters.any;
+    let intent = JSON.parse(request.body).queryResult.intent.displayName
+    error = (err) => {return response.send({"fulfillmentText":"Uh oh, ha habido un error!"});}
 
-    console.log('request',request); // Log the request envelope passed
-    // Query parameters passed are parsed into the request.params object for you
-    // console.log(paramsObject.a) // This would print "5" for query string "a=5
+    // Dada una entidad (QXXXX) y una propiedad (PXXXX), devuelve el label de la propiedad para la entidad
+    // o la lista de labels en el caso de que sea una lista
+    function getPropiedad(entidad, propiedad){
+	// TODO: Implementar
+	return false;
+    }
 
-    // Set the status code - by default it would return 200
-    response.status = 200;
-    // Set the headers the way you like
-    response.headers['X-Custom-Header'] = 'CustomHeaderValue';
-    
+    // Determina si QXXXXX es una obra de arte
+    function isArt(title){
+        return xhr.fetch("https://www.wikidata.org/wiki/Special:EntityData/"+title+".json").then((x) => {
+            let properties = JSON.parse(x.body).entities[title].claims;
+            // TODO: deducir a partir de properties que se trata de una obra de arte
+            return true;
+        }).catch({x} => {return x;});
+    }
+
+    // Selecciona resultado de la búsqueda
+    function selectResult(results){
+	return results.map((r) => {return r.title}).find(isArt) // primero que es obra de arte
+    }
+
+    // Busca entidad dado nombre
+    function buscaEntidad(nombre){
+	xhr.fetch("https://www.wikidata.org/w/api.php?action=wbsearchentities&search="+ any + "&language=es&limit=5&format=json")
+	    .then((x) => {return selectResult(JSON.parse(x.body).query.search);});}
+
+
+
     // Mapa de las funciones que gestionan cada intent
     let intentMap = new Map();
     intentMap.set('ObraRelacionada', obraRelacionada);
-    
+
     // Función que gestiona el intent ObraRelacionada
     function obraRelacionada(title){
         return title
     }
-    
-    // Función que determina si la página de Wikidata con un cierto título (QXXXXX) es una obra de arte
-    function isArt(title){
-        return xhr.fetch("https://www.wikidata.org/wiki/Special:EntityData/"+title+".json").then((x) => {
-            // handle server response
-            let properties = JSON.parse(x.body).entities[title].claims;
-            // TODO: deducir a partir de properties que se trata de una obra de arte
-            return true;
-        }).catch((err) => {
-            // handle request failure
-            return response.send("Malformed JSON body.");
-        });
-    }
-    
-    // Esta función es la que debe seleccionar uno de los resultados de la búsqueda
-    function selectResult(results){
-        // Devuelve el primer título que corresponde a una obra de arte
-        return results.map((r) => {return r.title}).find(isArt)
-    }
-    
-    let any = JSON.parse(request.body).queryResult.parameters.any;
-    let intent = JSON.parse(request.body).queryResult.intent.displayName;
-    
-    return xhr.fetch("https://www.wikidata.org/w/api.php?action=query&list=search&format=json&srsearch=" + any.split(" ").join("+")).then((x) => {
-        // handle server response
-        let result = selectResult(JSON.parse(x.body).query.search);
+
+    return buscaEntidad(any).then((result) => {
         return response.send({"fulfillmentText":intentMap.get(intent)(result)});
-    }).catch((err) => {
-        // handle request failure
-        return response.send("Malformed JSON body.");
-    });
+    }).catch(error);
 };
