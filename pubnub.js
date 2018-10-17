eb94e1fcf51114043c54b4c1ce6db079ca5a5553
Francisco J. Morales <fjmoralespiqueras@gmail.com>
2018-10-17 10:15:38 +0200

    Arregla cosas

--- pubnub.js
+++ pubnub.js
@@ -1,8 +1,15 @@
 export default (request, response) => {
+    const pubnub = require('pubnub');
+    const kvstore = require('kvstore');
     const xhr = require('xhr');
+    
+    let headersObject = request.headers;
+    let paramsObject = request.params;
+    let methodString = request.method;
+    let bodyString = request.body;
     let any = JSON.parse(request.body).queryResult.parameters.any;
-    let intent = JSON.parse(request.body).queryResult.intent.displayName
-    error = (err) => {return response.send({"fulfillmentText":"Uh oh, ha habido un error!"});}
+    let intent = JSON.parse(request.body).queryResult.intent.displayName;
+    let error = (err) => {return response.send({"fulfillmentText":"Uh oh, ha habido un error!"});};
 
     // Dada una entidad (QXXXX) y una propiedad (PXXXX), devuelve el label de la propiedad para la entidad
     // o la lista de labels en el caso de que sea una lista
@@ -17,18 +24,21 @@ export default (request, response) => {
             let properties = JSON.parse(x.body).entities[title].claims;
             // TODO: deducir a partir de properties que se trata de una obra de arte
             return true;
-        }).catch({x} => {return x;});
+        });
     }
 
     // Selecciona resultado de la búsqueda
     function selectResult(results){
-	return results.map((r) => {return r.title}).find(isArt) // primero que es obra de arte
+	return results.map((r) => {return r.title}).find(isArt); // primero que es obra de arte
     }
 
     // Busca entidad dado nombre
     function buscaEntidad(nombre){
-	xhr.fetch("https://www.wikidata.org/w/api.php?action=wbsearchentities&search="+ any + "&language=es&limit=5&format=json")
-	    .then((x) => {return selectResult(JSON.parse(x.body).query.search);});}
+	    return xhr.fetch("https://www.wikidata.org/w/api.php?action=wbsearchentities&search="+ nombre + "&language=es&limit=5&format=json")
+	    .then((x) => {
+	        let result = selectResult(JSON.parse(x.body).search);
+	        return result;
+	    });}
 
 
 
@@ -38,7 +48,7 @@ export default (request, response) => {
 
     // Función que gestiona el intent ObraRelacionada
     function obraRelacionada(title){
-        return title
+        return title;
     }
 
     return buscaEntidad(any).then((result) => {
